6d23b6e50d4f8a80761f17bfb322ce8a505a1aa0
erjemin <erjemin@gmail.com>
2026-01-14 12:14:54 +0300

    mod: minor

--- etpgrf_site/etpgrf_site/urls.py
+++ etpgrf_site/etpgrf_site/urls.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('typograph.urls')),
+    path(route='adm-in/', view=admin.site.urls),
+    path(
+        route="favicon.ico",
+        view=RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+        name="favicon",
+    ),
+    path(route='', view=include('typograph.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + urlpatterns
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- etpgrf_site/typograph/urls.py
+++ etpgrf_site/typograph/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('process/', views.process_text, name='process_text'),
+    path(route='', view=views.index, name='index'),
+    path(route='process/', view=views.process_text, name='process_text'),
 ]
