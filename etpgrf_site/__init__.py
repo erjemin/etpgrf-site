e21920262d7a8056f2060007ca6f78ba8e10c134
erjemin <erjemin@gmail.com>
2026-01-21 01:05:44 +0300

    mod: __init__

---
+++ etpgrf_site/__init__.py
@@ -0,0 +1,9 @@
+"""сайт etpgrf - сайт на django для тестирования и представления библиотеки экранной типографики etpgrf.
+Основные возможности:
+- Веб-интерфейс для ввода текста и настройки параметров типографики.
+"""
+__version__ = "0.1.3"
+__author__ = "Sergei Erjemin"
+__email__ = "erjemin@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2026 Sergei Erjemin"
