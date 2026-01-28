fedfae1f74f76482a6f554d0ee112488c703b635
erjemin <erjemin@gmail.com>
2026-01-28 20:27:10 +0300

    mod: песочница только в режиме debug

--- etpgrf_site/blog/urls.py
+++ etpgrf_site/blog/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
+from django.conf import settings
 from . import views
-from blog.models import PostType # Для использования в корневом urls.py
 
 app_name = 'blog' # Пространство имен для приложения blog
 
 urlpatterns = [
     # Лента блога: /blog/
     path('', views.post_list, name='post_list'),
-    
-    # Песочница для верстки: /blog/tmp/
-    path('tmp/', views.tmp_view, name='tmp'),
-    
-    # Детальная страница поста: /blog/my-awesome-post/
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
+
+# Песочница для верстки: /blog/tmp/
+# Добавляем ТОЛЬКО если DEBUG=True и ПЕРЕД post_detail
+if settings.DEBUG:
+    urlpatterns.append(path('tmp/', views.tmp_view, name='tmp'))
+
+# Детальная страница поста: /blog/my-awesome-post/
+# Этот маршрут должен быть последним, так как он перехватывает всё, что похоже на slug
+urlpatterns.append(path('<slug:slug>/', views.post_detail, name='post_detail'))
